06536bb82fb72ebc7bc24285c1735501475fd41f
Morad Edwar <me@morad-edwar.com>
2016-05-02 14:52:52 +0200

    Views Utility Class

--- app/src/main/java/com/morxander/coins/ViewsUtility.java
+++ app/src/main/java/com/morxander/coins/ViewsUtility.java
@@ -1,7 +1,28 @@
 package com.morxander.coins;
 
+import android.content.Context;
+import android.graphics.Typeface;
+import android.widget.EditText;
+import android.widget.TextView;
+
 /**
  * Created by morxander on 5/2/16.
  */
 public class ViewsUtility {
+
+    private static Typeface getTypeFace(Context context){
+        Typeface face = Typeface.createFromAsset(context.getAssets(), "fonts/Ubuntu-BI.ttf");
+        return face;
+    }
+
+    public static void changeTypeFace(Context context, EditText editText){
+        Typeface face = getTypeFace(context);
+        editText.setTypeface(face);
+    }
+
+    public static void changeTypeFace(Context context, TextView textView){
+        Typeface face = getTypeFace(context);
+        textView.setTypeface(face);
+    }
+
 }
